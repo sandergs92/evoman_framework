eeb716c71d540ac88789de49059cb154a41b8549
Sander <sandergs@protonmail.com>
2022-09-15 12:54:18 +0200

    Preparing NEAT controller for co-evolution, enemy_controller implemented

--- neat_controller.py
+++ neat_controller.py
@@ -30,3 +30,29 @@ class player_controller(Controller):
         else:
             release = 0
         return [left, right, jump, shoot, release]
+
+# implements controller structure for enemy
+class enemy_controller(Controller):
+    def control(self, inputs,controller):
+        output = controller.activate(inputs)
+        # takes decisions about sprite actions
+        if output[0] > 0.5:
+            attack1 = 1
+        else:
+            attack1 = 0
+
+        if output[1] > 0.5:
+            attack2 = 1
+        else:
+            attack2 = 0
+
+        if output[2] > 0.5:
+            attack3 = 1
+        else:
+            attack3 = 0
+            
+        if output[3] > 0.5:
+            attack4 = 1
+        else:
+            attack4 = 0
+        return [attack1, attack2, attack3, attack4]
